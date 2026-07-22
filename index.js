cfc90a7c5fd360b5a7ffef6cba32c310a667af1e
natasha9999 <alvesna9999@gmail.com>
2026-07-22 19:17:35 -0300

    Este é um commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Olá mundo!!!!!')
+
+// git init
+// git add nomeDoArquivo ou pra enviar todos os arquivos que estão na pasta:  git add .
+// comitar o codigo: salva e registra o que foi feito no código até este momento. git commit -m "mensagem"
+// Renomear a branch: git branch -M main
+// git remote add origin https://github.com/natasha9999/desafio-logica-inicial.git
+// Enviando os arquivos pro Github:  git push -u nomeDoLinkRemoto nomeDaBranch portanto: git push -u origin main
